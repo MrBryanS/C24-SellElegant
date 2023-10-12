5dfa8b776c489bf7e9616746805b00aa8cecf470
BradyLange1 <bradylange99@gmail.com>
2023-10-12 15:41:19 -0500

    added login functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,36 @@
 const { User, Product } = require("../models");
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    user: async () => {
+    users: async () => {
       return User.find({});
     },
-    product: async () => {
+    user: async () => {
+      return User.findOne({ email });
+    },
+    products: async () => {
       return Product.find({});
+    },
+    product: async () => {
+      return Product.findOne({ _id });
     },
   },
   Mutation: {
-    createUser: async (parents, {email, password}) => {
-      const user = await User.create({email, password});
+    createUser: async (parents, { email, password }) => {
+      const user = await User.create({ email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPassword = await user.isCorrectPassword(password);
+      if (!correctPassword) {
+        throw AuthenticationError;
+      }
       const token = signToken(user);
       return { token, user };
     },
