7818cce30ba4f22bcf48c9f08afdebe5dc0c9266
Bryan Stauning <bryanstauning@gmail.com>
2023-10-12 19:54:42 -0500

    User model completed

--- server/models/User.js
+++ server/models/User.js
@@ -2,18 +2,35 @@ const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true, // removes whitespace
+  },
+  
   email: {
     type: String,
     required: true,
     unique: true,
     match: [/.+@.+\..+/, "Must match an email address!"],
   },
+   
   // create a validator here - look up online
   password: {
     type: String,
     required: true,
     minlength: 5,
   },
+  orderCount: {
+    type: Number,
+    default: 0,
+  },
+  savedOrders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
 });
 
 userSchema.pre("save", async function (next) {
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,30 @@ const typeDefs = `#graphql
 #This is a GraphQL schema for the application
 
   type User {
-    email: String!
+    _id: ID!
+    username: String
+    email: String
     password: String!
+    orderCount: Int #like bookCount from c21
+    savedOrders: [ Order ]  #like savedBooks from c21   
+    
   }
 
   type Product {
+    _id: ID!
     product_name: String!
-    price: Float!
-    stock: Int!
+    productDescription: String!
+    price: Float    
   }
+
+#Order is the list of orders in the database - like book type in c21 (books saved by the user)
+  type Order {
+    _id: ID!
+    products: [ Product ] #like authors from c21
+    orderDate: String!
+    orderShipped: Bollean!  #true is shipped, false is pending
+    
+}
 
   # type Auth
   type Auth {
