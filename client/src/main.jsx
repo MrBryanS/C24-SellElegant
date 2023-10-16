113b0bec2ceec08e87ed227c2c6d9b6d963bbec8
Bryan Stauning <bryanstauning@gmail.com>
2023-10-16 16:36:00 -0500

    remove import App.css on Main.jsx - duplicate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import "./App.css";
 
 import App from "./App.jsx";
 import Account from "./pages/Account";
