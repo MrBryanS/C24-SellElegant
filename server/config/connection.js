bbcf979367398c567fadea315e4eeb64904c3690
Bryan Stauning <bryanstauning@gmail.com>
2023-10-16 19:50:58 -0500

    fix typo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(h || 'mongodb://127.0.0.1:27017/sellelegant');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sellelegant');
 
 module.exports = mongoose.connection;
